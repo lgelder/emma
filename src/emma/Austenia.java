419766945f1e88a25ee5d60e0e65b17e36b403f8
lgelder <lgelder13@gmail.com>
2016-01-12 22:25:09 -0500

    Estate selection, multiple questions given

--- src/emma/Austenia.java
+++ src/emma/Austenia.java
@@ -1,97 +1,120 @@
 package emma;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Austenia{
 	
-	public static List<Option> createTQOptions(String one, String two, String three){
-		Option b = new Option(1, one);
-		Option c = new Option(2, two);
-		Option d = new Option(3, three);
+	public static List<Option> createTQOptions(List<String> list){
 		List<Option> questionchoices = new ArrayList();
-		questionchoices.add(b);
-		questionchoices.add(c);
-		questionchoices.add(d);
+		for (int i = 0; i < list.size(); i++) {
+			questionchoices.add( new Option((i+1), list.get(i)));
+		}
 		return questionchoices;
 	}
 	
 	public static List<TriviaQuestion> createTQs(){
+//		File myFile = new File("TriviaQuestions.txt");
+//		try (BufferedReader br = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(myFile))))) {
+//		    String line;
+//		    while ((line = br.readLine()) != "\r") {
+//		       // process the line.
+//		    }
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		
 		List<TriviaQuestion> listOfQuestions = new ArrayList<TriviaQuestion>();
 		
-		List<Option> listOfOptions1 = createTQOptions("Mr. Perry", "Mr. Elton", "Mr. Knightley");
+		List<String> listOfOptions0 = new ArrayList<String>();
+		listOfOptions0.add("Mr. Perry");
+		listOfOptions0.add("Mr. Elton");
+		listOfOptions0.add("Mr. Knightley");
+
+		List<Option> listOfOptions1 = createTQOptions(listOfOptions0);
 		TriviaQuestion question1 = new TriviaQuestion("Mr. Woodhouse's Question", 
-				"Answer the multiple choice question.", "What is the name of Mr. Woodhouse's doctor?", 
+			 "What is the name of my doctor?", 
 				"Mr. Perry", listOfOptions1);
 		listOfQuestions.add(question1);
+
+		List<String> listOfOptions = new ArrayList<String>();
+		listOfOptions.add("Elizabeth");
+		listOfOptions.add("Augusta");
+		listOfOptions.add("Jane");
+		listOfOptions.add("Georgiana");
 		
-		List<Option> listOfOptions2 = createTQOptions("Catherine", "Augusta", "Elizabeth");
-		TriviaQuestion question2 = new TriviaQuestion("Mrs. Weston's Question", 
-				"Answer the multiple choice question.", "What is Mrs. Elton's first name?", 
+		List<Option> listOfOptions2 = createTQOptions(listOfOptions);
+		TriviaQuestion question2 = new TriviaQuestion("Mrs. Elton Question", 
+				"What is my first name?", 
 				"Augusta", listOfOptions2);
-		listOfQuestions.add(question2);
-		
-		List<Option> listOfOptions3 = createTQOptions("Mr. Perry", "Mr. Elton", "Mr. Knightley");
-		TriviaQuestion question3 = new TriviaQuestion("Mr. Woodhouse's Question", 
-				"Answer the multiple choice question.", "What is the name of Mr. Woodhouse's doctor?", 
-				"Mr. Perry", listOfOptions3);
-		listOfQuestions.add(question3);
-		
+		listOfQuestions.add(question2);		
 		
 		return listOfQuestions;
 	}
 	
-	
-	
-	public static void main(String[] a){
-		
+	public static Locale setUp(){
 		List<TriviaQuestion> listOfQuestions = Austenia.createTQs();
-				
+		
 		Person mrWoodhouse = new Person("Mr. Woodhouse", listOfQuestions.get(0));
 		
-		Person mrsWeston = new Person("Mrs. Weston", listOfQuestions.get(1));
+		Person mrsElton = new Person("Mrs. Elton", listOfQuestions.get(1));
 		
 		Locale hartfield = new Locale("Hartfield");
 		hartfield.addPerson(mrWoodhouse);
-		hartfield.addPerson(mrsWeston);
-		System.out.println(hartfield.getPeople().get(0).getTriviaQuestion().printQuestion());
+		hartfield.addPerson(mrsElton);
+		return hartfield;
+	}
+	
+	public static void runCity(Locale l){
+		for (int i=0; i < 2; i++){
+			System.out.println("Hello, my name is " + l.getPeople().get(i).getName() + ".\n" + l.getPeople().get(i).getTriviaQuestion().printQuestion());
+			boolean b = checkAnswer(l.getPeople().get(i).getTriviaQuestion());
+			interpretAnswer(b);
+		}
+	}
+	
+	public static void main(String[] a){
 		
-//		System.out.println(mrWoodhouse.getTriviaQuestion().printQuestion());
+		String welcome = "Welcome to Austenia. Select an estate number to get started: \n1. Hartfield";
+		String thanksForPlaying = "Thank you for visiting Austenia. We hope you come back soon.";
+		System.out.println(welcome);
+		Scanner readIn = new Scanner(System.in);
+		String selection = readIn.nextLine();
+		if (selection == "1"){
+			Locale hartfield = setUp();
+			runCity(hartfield);
+		}
+		Locale hartfield = setUp();
+		runCity(hartfield);
+		System.out.println(thanksForPlaying);
 		
-		
-	    Scanner readIn = new Scanner(System.in);
+	}
+	public static boolean checkAnswer(TriviaQuestion tq){
+		Scanner readIn = new Scanner(System.in);
 	    String userChoice = readIn.nextLine();
 	    int userString = Integer.parseInt(userChoice);
 	    System.out.println("You chose number " + userChoice + ", " + 
-	    		mrWoodhouse.getTriviaQuestion().getChoices().get(userString-1).getText() + ".");
+	    		tq.getChoices().get(userString-1).getText() + ".");
 	    
-	    if (mrWoodhouse.getTriviaQuestion().getAnswer() == 
-	    		mrWoodhouse.getTriviaQuestion().getChoices().get(userString-1).getText()){
-	    	System.out.println("you are correct!\n");
-	    }else{
-	    	System.out.println("Oops, that's wrong.\n");
+	    if (tq.getAnswer() == tq.getChoices().get(userString-1).getText()){
+	    	return true;
 	    }
+	    return false;
 	    
-		System.out.println(hartfield.getPeople().get(1).getTriviaQuestion().printQuestion());
-
-//		System.out.println(mrsWeston.getTriviaQuestion().printQuestion());
-		
-		
-	    userChoice = readIn.nextLine();
-	    userString = Integer.parseInt(userChoice);
-	    readIn.close();            
-	    System.out.println("You chose number " + userChoice + ", " + 
-	    		mrsWeston.getTriviaQuestion().getChoices().get(userString-1).getText() + ".");
-	    
-	    if (mrsWeston.getTriviaQuestion().getAnswer() == 
-	    		mrsWeston.getTriviaQuestion().getChoices().get(userString-1).getText()){
-	    	System.out.println("you are correct!");
-	    }else{
-	    	System.out.println("Oops, that's wrong.");
-	    }
-	    
-//		System.out.println(mrWoodhouse.getTriviaQuestion().getTitle());
 	}
-
+		public static void interpretAnswer(boolean b){
+			if (b == true){
+				System.out.println("Congratulations! You are correct!\n");
+			}else{
+				System.out.println("Sorry, that's incorrect.\n");
+			}
+			
+		}
 }
